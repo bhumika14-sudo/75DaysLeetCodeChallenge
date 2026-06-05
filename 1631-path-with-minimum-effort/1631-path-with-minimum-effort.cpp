a971a7b5b56537eedf1d462181504ab43fccdbe6
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2026-06-05 11:31:02 +0530

    Time: 56 ms (54.51%), Space: 24.5 MB (77.28%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -4,7 +4,6 @@ public:
     int dy[4] = {0, 1, -1, 0};
 
     int minimumEffortPath(vector<vector<int>>& heights) {
-
         int n = heights.size();
         int m = heights[0].size();
 
@@ -13,14 +12,12 @@ public:
             vector<pair<int, pair<int,int>>>,
             greater<pair<int, pair<int,int>>>
         > pq;
-
         vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
 
         dist[0][0] = 0;
         pq.push({0, {0, 0}});
 
         while(!pq.empty()) {
-
             auto pr = pq.top();
             pq.pop();
 
@@ -30,26 +27,20 @@ public:
 
             if(dist_of_par > dist[x][y])
                 continue;
-
             if(x == n - 1 && y == m - 1)
                 return dist_of_par;
 
             for(int k = 0; k < 4; k++) {
-
                 int ii = x + dx[k];
                 int jj = y + dy[k];
 
                 if(ii < 0 || jj < 0 || ii >= n || jj >= m)
                     continue;
-
                 int new_dist =
                     max(dist_of_par,
                         abs(heights[x][y] - heights[ii][jj]));
-
                 if(new_dist < dist[ii][jj]) {
-
                     dist[ii][jj] = new_dist;
-
                     pq.push({new_dist, {ii, jj}});
                 }
             }
