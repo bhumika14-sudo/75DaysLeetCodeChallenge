af1224629fa756e1dea8b329ae1339bab8e4e0ef
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2026-07-02 14:22:56 +0530

    Time: 46 ms (5.2%), Space: 44.7 MB (78.02%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,40 +1,21 @@
-// class Solution {
-// public:
-//     int findContentChildren(vector<int>& g, vector<int>& s) {
-//         sort(g.begin(), g.end());
-//         sort(s.begin(), s.end());
-
-//         int i = g.size() - 1; // largest greed child
-//         int j = s.size() - 1; // largest cookie
-//         int count = 0;
-//         while (i >= 0 && j >= 0) {
-//             if (s[j] >= g[i]) {
-//                 count++;
-//                 i--;
-//                 j--;
-//             } else {
-//                 // largest cookie bhi is child ko satisfy nahi kar sakti
-//                 i--;
-//             }
-//         }
-//         return count;
-//     }
-// };
-
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
 
-        int i = 0; // child pointer
-        int j = 0; // cookie pointer
-        while (i < g.size() && j < s.size()) {
-            if (s[j] >= g[i]) {
-                i++; // child satisfied
+        int i=0, j=0, count=0;
+
+        while(i<g.size() && j<s.size()){
+            if(s[j] >= g[i]){
+                count++;
+                i++;
+                j++;
             }
-            j++; // move to next cookie
+            else{
+                j++;
+            }
         }
-        return i;
+        return count;
     }
-};        
+};
