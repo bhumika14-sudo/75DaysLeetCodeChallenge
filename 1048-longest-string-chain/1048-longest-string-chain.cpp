7fe15f1cd0ebc121a24f74a9c45f38edd0a3eeaf
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2026-07-05 20:12:27 +0530

    Time: 103 ms (38.86%), Space: 106 MB (28.65%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -4,12 +4,13 @@ public:
         return a.size() < b.size();
     }
 
-    bool check(string &small, string &large){
-        if(small.size()+1 != large.size()) return false;
-    
-        int i=0, j=0;
-        while(j < large.size()){
-            if(i < small.size() && small[i] == large[j]){
+    bool isPredecessor(string &a, string &b){
+        if(b.size() != a.size() + 1)
+            return false;
+        int i = 0, j = 0;
+
+        while(i < a.size() && j < b.size()){
+            if(a[i] == b[j]){
                 i++;
                 j++;
             }
@@ -17,23 +18,43 @@ public:
                 j++;
             }
         }
-        return i==small.size(); 
+        return i == a.size();
     }
+
+    // int solve(int i, int prev, vector<string>& words, vector<vector<int>>& dp){
+    //     int n = words.size();
+
+    //     if(i == n)
+    //         return 0;
+
+    //     if(dp[i][prev + 1] != -1)
+    //         return dp[i][prev + 1];
+
+    //     int notTake = solve(i + 1, prev, words, dp);
+    //     int take = 0;
+    //     if(prev == -1 || isPredecessor(words[prev], words[i])){
+    //         take = 1 + solve(i + 1, i, words, dp);
+    //     }
+    //     return dp[i][prev + 1] = max(take, notTake);
+    // }
+
     int longestStrChain(vector<string>& words) {
         sort(words.begin(), words.end(), cmp);
         int n = words.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
 
-        vector<int> dp(n,1);
-        int ans = 1;
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(check(words[j], words[i]) && dp[j]+1 > dp[i]){
-                    dp[i] = dp[j]+1;
+        for(int i=n-1;i>=0;i--){
+            for(int prev=i-1;prev>=-1;prev--){
+                int notTake = dp[i+1][prev+1];
+                int take = 0;
+                if(prev==-1 || isPredecessor(words[prev],words[i])){
+                    take = 1 + dp[i+1][i+1];
                 }
+                dp[i][prev+1] = max(take,notTake);
             }
-            ans= max(ans, dp[i]);
         }
-        return ans;
+        return dp[0][0];
+        
+        // return solve(0, -1, words, dp);
     }
 };
