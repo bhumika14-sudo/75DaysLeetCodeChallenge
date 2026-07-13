cd547a160eb07062934ae098c7a7550ee0d91956
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2026-07-13 15:41:52 +0530

    Time: 0 ms (100%), Space: 10 MB (11.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-        unordered_map<char,int> mp;
+        if(s.size() != t.size()) return false;
+        unordered_map<char, int> mp;
 
-        for(char c:s){
-            mp[c]++;
+        for(char ch : s){
+            mp[ch]++;
         }
-        for(char c:t){
-            mp[c]--;
 
-            if(mp[c]<0){
+        for(char ch : t){
+            mp[ch]--;
+
+            if(mp[ch] < 0){
                 return false;
             }
         }
         return true;
-    }    
+    }
 };
