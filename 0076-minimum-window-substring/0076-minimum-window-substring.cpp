ced5e8a56faa2ee8b2b74682f31360c490af3a62
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2026-08-18 11:27:18 +0530

    Time: 11 ms (47.39%), Space: 11.4 MB (83.43%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,16 +3,17 @@ public:
     string minWindow(string s, string t) {
         int n = s.size();
         int m = t.size();
-        unordered_map<char,int> mp;
+        unordered_map<char, int> mp;
         for(char ch : t){
             mp[ch]++;
         }
+
         int left = 0;
         int right = 0;
         int cnt = 0;
         int minLen = INT_MAX;
         int sIdx = -1;
-
+        
         while(right < n){
             if(mp[s[right]] > 0){
                 cnt++;
@@ -20,11 +21,11 @@ public:
             mp[s[right]]--;
             while(cnt == t.size()){
                 if(right - left + 1 < minLen){
-                    minLen = right - left + 1;
+                    minLen = right-left+1;
                     sIdx = left;
                 }
                 mp[s[left]]++;
-                if(mp[s[left]] > 0){
+                if(mp[s[left]]>0){
                     cnt--;
                 }
                 left++;
