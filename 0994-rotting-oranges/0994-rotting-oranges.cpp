f5a2fd36b985e2eeeeb8cf270fe80cb5251f78b1
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2026-08-19 16:02:31 +0530

    Time: 0 ms (100%), Space: 16.7 MB (86.5%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,44 +1,48 @@
 class Solution {
 public:
-    int dx[4] = {-1,0,0,1};
-    int dy[4] = {0,-1,1,0};
-    int a,b;
+    int nr[4] = {-1,0,0,1};
+    int nc[4] = {0,-1,1,0};
     int orangesRotting(vector<vector<int>>& grid) {
-        a = grid.size();
-        b = grid[0].size();
-
-        int ans=0;
+        int n = grid.size();
+        int m = grid[0].size();
         queue<pair<int,int>> q;
-        for(int i=0;i<a;i++){
-            for(int j=0;j<b;j++){
-                if(grid[i][j]==2){
+
+        int fresh = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 2){
                     q.push({i,j});
+                }
+                else if(grid[i][j] == 1){
+                    fresh++;
                 }
             }
         }
 
-        while(!q.empty()){
+        int time = 0;
+        while(!q.empty() && fresh>0){
             int size = q.size();
-            int temp=0;
             while(size--){
                 auto node = q.front();
                 q.pop();
-                for(int k=0;k<4;k++){
-                    int i=node.first+dx[k] , j=node.second+dy[k];
-                    if(i>=0 && j>=0 && i<a && j<b && grid[i][j]==1){
-                        grid[i][j]=2;
-                        temp=1;
-                        q.push({i,j});
+
+                int x = node.first;
+                int y = node.second;
+
+                for(int k=0; k<4; k++){
+                    int newX = x + nr[k];
+                    int newY = y + nc[k];
+
+                    if(newX>=0 && newX<n && newY>=0 && newY<m && grid[newX][newY] == 1){
+                        grid[newX][newY] = 2;
+                        q.push({newX, newY});
+                        fresh--;
                     }
                 }
             }
-            ans+=temp;
+            time++;
         }
-        for(auto v:grid){
-            for(int x:v){
-                if(x==1) return -1;
-            }
-        }
-        return ans;
+        if(fresh > 0) return -1;
+        return time;
     }
 };
