d725b9c56e413e15ca21e6bf069030402e8330bd
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2026-08-19 14:30:47 +0530

    Time: 4 ms (66.42%), Space: 23.8 MB (59.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        int m = intervals[0].size();
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-        for(int i = 1; i < intervals.size(); i++) {
-            // Overlap
-            if(intervals[i][0] <= ans.back()[1]) {
 
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-            }
-            // No overlap
-            else {
+        ans.push_back(intervals[0]);
+
+        for(int i=1; i<n; i++){
+            //overlap
+            if(intervals[i][0] <= ans.back()[1]){
+                ans.back()[1] = max(ans.back()[1] , intervals[i][1]);
+            }        
+            //not overlap
+            else{
                 ans.push_back(intervals[i]);
             }
-        }
+        }    
         return ans;
     }
 };
