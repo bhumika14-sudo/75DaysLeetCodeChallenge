fa7bce91b612b3d84e6ced89842a74092fb04517
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2026-09-21 15:17:12 +0530

    Time: 0 ms (100%), Space: 10.3 MB (73.58%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,40 +1,72 @@
 class Solution {
 public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'A' ||
+               c == 'e' || c == 'E' ||
+               c == 'i' || c == 'I' ||
+               c == 'o' || c == 'O' ||
+               c == 'u' || c == 'U';
+    }
     string reverseVowels(string s) {
-        int n = s.size();
+        int i = 0;
+        int j = s.size() - 1;
 
-        vector<char> vowels;
+        while(i < j) {
 
-        // Saare vowels store karo
-        for(int i = 0; i < n; i++) {
-            if(s[i] == 'a' || s[i] == 'A' ||
-               s[i] == 'e' || s[i] == 'E' ||
-               s[i] == 'i' || s[i] == 'I' ||
-               s[i] == 'o' || s[i] == 'O' ||
-               s[i] == 'u' || s[i] == 'U') {
-                
-                vowels.push_back(s[i]);
-            }
-        }
+            while(i < j && !isVowel(s[i]))
+                i++;
 
-        // Vowels ka order reverse
-        reverse(vowels.begin(), vowels.end());
+            while(i < j && !isVowel(s[j]))
+                j--;
 
-        // Original string mein replace
-        int j = 0;
+            swap(s[i], s[j]);
 
-        for(int i = 0; i < n; i++) {
-            if(s[i] == 'a' || s[i] == 'A' ||
-               s[i] == 'e' || s[i] == 'E' ||
-               s[i] == 'i' || s[i] == 'I' ||
-               s[i] == 'o' || s[i] == 'O' ||
-               s[i] == 'u' || s[i] == 'U') {
-
-                s[i] = vowels[j];
-                j++;
-            }
+            i++;
+            j--;
         }
 
         return s;
     }
 };
+
+
+// class Solution {
+// public:
+//     string reverseVowels(string s) {
+//         int n = s.size();
+
+//         vector<char> vowels;
+
+//         // Saare vowels store karo
+//         for(int i = 0; i < n; i++) {
+//             if(s[i] == 'a' || s[i] == 'A' ||
+//                s[i] == 'e' || s[i] == 'E' ||
+//                s[i] == 'i' || s[i] == 'I' ||
+//                s[i] == 'o' || s[i] == 'O' ||
+//                s[i] == 'u' || s[i] == 'U') {
+                
+//                 vowels.push_back(s[i]);
+//             }
+//         }
+
+//         // Vowels ka order reverse
+//         reverse(vowels.begin(), vowels.end());
+
+//         // Original string mein replace
+//         int j = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             if(s[i] == 'a' || s[i] == 'A' ||
+//                s[i] == 'e' || s[i] == 'E' ||
+//                s[i] == 'i' || s[i] == 'I' ||
+//                s[i] == 'o' || s[i] == 'O' ||
+//                s[i] == 'u' || s[i] == 'U') {
+
+//                 s[i] = vowels[j];
+//                 j++;
+//             }
+//         }
+
+//         return s;
+//     }
+// };
